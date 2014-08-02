eba2ca4d804ec1ba46f9bc67697635ffa4e0a5bb
wanghuan <654296217@qq.com>
2014-08-02 16:09:15 +0800

    pos_v1 is ok

--- pos_v1/src/main.js
+++ pos_v1/src/main.js
@@ -1,110 +1,143 @@
-//TODO: Please write code in this file.
-
-
-
 function printInventory(inputs)
 {
-var    allItems=loadAllItems();
-var item_sum=[];
-//item_sum=0;
+  var allItems=loadAllItems();
+  var item_sum=new Array();
+  for(var i=0;i<allItems.length;i++)
+       item_sum[i]=0;
+  for (var i=0;i<inputs.length;i++)
+    {
+      for (var j=0;j<allItems.length;j++)
+        {
+          if(inputs[i].substring(0,10)==allItems[j].barcode)
+          {
+            if(inputs[i].length>10)
+              item_sum[j]=inputs[i].substring(11,inputs[i].length);
+            else
+              ++item_sum[j];
+          }
+        }
+    }
+  var string_1=[];
+  var string_2=[];
+  string_1[0]='***<没钱赚商店>购物清单***\n';
+  string_1='***<没钱赚商店>购物清单***\n';
+  string_2='挥泪赠送商品：\n' ;
+  var promotion=loadPromotions();
+  var num_1,num_2,num;
+  var sum_1=0;
+  var sum_2=0;
 
-for (var i=0;i<inputs.length;i++)
-  {
-    for (var j=0;j<allItems.length;j++)
+var discount_goods_num=new Array();
+for(var i=0;i<allItems.length;i++)
+     discount_goods_num[i]=0;
+
+  for(i=0;i<allItems.length;i++)
+    {
+      if(item_sum[i]!=0)
       {
-        if(inputs[i].substring(0,10)===allItems[j])
-          if(inputs[i].length>10)
-            item_sum[j]=inputs[i].substring(11,inputs[i].length);
-          else
-            ++item_sum[j];
+        for(j=0;j<promotion[0].barcodes.length;j++)
+          {
+
+            // string_1+='名称：'+allItems[i].name +', 数量 : '+item_sum[i]+allItems[i].unit+
+            //           ', 单价:'+allItems[i].price.toFixed(2)+'(元)'+'， 小计：'+
+            //           (item_sum[i]*allItems[i].price).toFixed(2)+'(元)\n';
+
+            if(allItems[i].barcode==promotion[0].barcodes[j])
+            {
+                num_1=Math.floor(item_sum[i]/3|0);
+                // num_2=item_sum[i]%3;
+                // num=num_1*2+num_2;
+                discount_goods_num[i]=num_1;
+                // item_sum[i]=num;
+                if(num_1>=1)
+                {
+                  string_2+='名称：'+allItems[i].name +'，数量：'+num_1+allItems[i].unit+'\n';
+                  sum_2+=allItems[i].price * num_1;
+                }
+                // string_1+='名称：'+allItems[i].name +', 数量 : '+item_sum[i]+allItems[i].unit+
+                //           ', 单价:'+allItems[i].price.toFixed(2)+'(元)'+'， 小计：'+
+                //           (item_sum[i]*allItems[i].price).toFixed(2)+'(元)\n';
+              }
+
+          }
+          // console.log(item[i]);
+          string_1+='名称：'+allItems[i].name +'，数量：'+item_sum[i]+allItems[i].unit+
+                     '，单价：'+allItems[i].price.toFixed(2)+'(元)，小计：'+
+                      ((item_sum[i]-discount_goods_num[i])*allItems[i].price).toFixed(2)+'(元)\n';
+          sum_1+=allItems[i].price * (item_sum[i]-discount_goods_num[i]);
+        }
       }
-  }
-
-
-var str_num_1=1;
-var str_num_2=2;
-var string_1=[];
-var string_2=[];
-
-string_1[0]='***<没钱赚商店>购物清单***\n';
-string_2[0]='挥泪赠送商品：\n' ;
-var promotion=loadPromotions();
-var pro_num=promotion[0].barcodes.length;
-
-for(i=0;i<allItems.length;i++)
-  {
-    if(item_sum[i]!=0)
-    {
-      string_1[str_num_1]='名称：'+allItems[i].name +'数量:'+item_sum+
-                        '单价:'+allItems[i].price+'(元)'+'小计：'+
-                        item_sum[i]*allItems[i].price;
-      judge_promotion();
-
-    }
-
-  }
-function  judge_promotion(){
-  for(j=0;j<pro_num;j++)
-    {
-      if(inputs[i]===promotion[0].barcodes[j])
-        {
-
-        }
-    }
-}
-}
-/*
-
-function loadAllItems() {
-    return [
-        {
-            barcode: 'ITEM000000',
-            name: '可口可乐',
-            unit: '瓶',
-            price: 3.00
-        },
-        {
-            barcode: 'ITEM000001',
-            name: '雪碧',
-            unit: '瓶',
-            price: 3.00
-        },
-        {
-            barcode: 'ITEM000002',
-            name: '苹果',
-            unit: '斤',
-            price: 5.50
-        },
-        {
-            barcode: 'ITEM000003',
-            name: '荔枝',
-            unit: '斤',
-            price: 15.00
-        },
-        {
-            barcode: 'ITEM000004',
-            name: '电池',
-            unit: '个',
-            price: 2.00
-        },
-        {
-            barcode: 'ITEM000005',
-            name: '方便面',
-            unit: '袋',
-            price: 4.50
-        }
-    ];
+     console.log(string_1+'----------------------\n' +string_2+'----------------------\n'+
+                 '总计：'+ sum_1.toFixed(2) +'(元)\n' +'节省：'+sum_2.toFixed(2)+'(元)\n'+'**********************');
 }
 
-function loadPromotions() {
-    return [
-        {
-            type: 'BUY_TWO_GET_ONE_FREE',
-            barcodes: [
-                'ITEM000000',
-                'ITEM000001',
-                'ITEM000005'
-            ]
-        }
-    ]
-}*/
+// var inputs = [
+//         'ITEM000001',
+//         'ITEM000001',
+//         'ITEM000001',
+//         'ITEM000001',
+//         'ITEM000001',
+//         'ITEM000003-2',
+//         'ITEM000005',
+//         'ITEM000005',
+//         'ITEM000005'
+//     ];
+// printInventory(inputs);
+//
+//
+//
+//
+//
+// function loadAllItems() {
+//     return [
+//         {
+//             barcode: 'ITEM000000',
+//             name: '可口可乐',
+//             unit: '瓶',
+//             price: 3.00
+//         },
+//         {
+//             barcode: 'ITEM000001',
+//             name: '雪碧',
+//             unit: '瓶',
+//             price: 3.00
+//         },
+//         {
+//             barcode: 'ITEM000002',
+//             name: '苹果',
+//             unit: '斤',
+//             price: 5.50
+//         },
+//         {
+//             barcode: 'ITEM000003',
+//             name: '荔枝',
+//             unit: '斤',
+//             price: 15.00
+//         },
+//         {
+//             barcode: 'ITEM000004',
+//             name: '电池',
+//             unit: '个',
+//             price: 2.00
+//         },
+//         {
+//             barcode: 'ITEM000005',
+//             name: '方便面',
+//             unit: '袋',
+//             price: 4.50
+//         }
+//     ];
+// }
+//
+// function loadPromotions() {
+//     return [
+//         {
+//             type: 'BUY_TWO_GET_ONE_FREE',
+//             barcodes: [
+//                 'ITEM000000',
+//                 'ITEM000001',
+//                 'ITEM000005'
+//             ]
+//         }
+//     ]
+// }
